2a995eef6194e9396bd7dba3c3c6645b9e84ff52
Katrina Sanford <blueyed_qt@hotmail.com>
2015-02-19 09:39:30 -0800

    Delete function is working through aajx

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,14 +1,24 @@
 class NotesController < ApplicationController
   def index
-  	@notes = Note.all
+  	@notes = Note.all.reverse
   	@note = Note.new
   end
 
   def create
   	@note = Note.create(title:params[:note][:title], description:params[:note][:description])
   	if @note.save
-  		json_message = {title:params[:note][:title], description:params[:note][:description]}
+  		@note_id = Note.select(:id).last
+  		json_message = {action:"create", title:params[:note][:title], description:params[:note][:description], id:@note_id}
   		render json: json_message
   	end
+  end
+
+  def edit
+  end
+
+  def destroy
+  	@note = Note.find(params[:id]).destroy
+  	json_message = {action:"success"}
+  	render json: json_message
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'notes#index'
-  post 'notes/create' => 'notes#create'
+  resources :notes, only: [:create, :update, :destroy]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
